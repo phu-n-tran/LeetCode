9e6889351d4a8bae3849d1fa337c5e6d165daed5
Phu Tran <andytran1997@gmail.com>
2020-05-17 21:45:44 -0700

    Update 5_17_FindAllAnagrams.py

--- monthlyChallenge/mayChallenge/5_17_FindAllAnagrams.py
+++ monthlyChallenge/mayChallenge/5_17_FindAllAnagrams.py
@@ -137,4 +137,36 @@ def findAnagrams(self, s, p):
             i = i + 1
             
         return ans
+        
+###################################################
+###########slower or equal solution################
+# Solution 1 : Sliding Window, Hash Table
+    def findAnagrams(self, s, p):
+        cnt_p = Counter(p); n = len(s); m = len(p); res = []
+        for i in range(n-m+1):
+            if i == 0: cnt_s = Counter(s[:m])
+            else:
+                prev = s[i-1]; curr = s[i+m-1]
+                cnt_s[prev] -= 1; cnt_s[curr] += 1
+                if cnt_s[prev] == 0: del cnt_s[prev]
+            if cnt_s == cnt_p: res.append(i)
+        return res        
+        
+		
+    # Solution 2 : Hash Table (Time Limit Exceeded)
+    def findAnagrams2(self, s, p): 
+        cnt_p = Counter(p); n = len(s); m = len(p); res = []
+        for i in range(n-m+1):
+            cnt_s = Counter(s[i:i+m])
+            if cnt_s == cnt_p: res.append(i)
+        return res        
+		
+        
+    # Solution 3 : Sort (Time Limit Exceeded)
+    def findAnagrams3(self, s, p):
+        p = sorted(p); n = len(s); m = len(p); res = []
+        for i in range(n-m+1):
+            q = sorted(s[i:i+m])
+            if q == p: res.append(i)
+        return res
 """
