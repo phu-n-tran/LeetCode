8e35556388f450daad5183b5821bcf195317e2e8
Phu Tran <andytran1997@gmail.com>
2020-05-22 14:45:36 -0700

    Create 4_5_MidOfLinkedList.py

---
+++ monthlyChallenge/aprilChallenge/4_5_MidOfLinkedList.py
@@ -0,0 +1,69 @@
+# --------------------------------------------------------------------------
+# Name:        Middle of the Linked List
+# Author(s):   Phu Tran
+# --------------------------------------------------------------------------
+"""
+    Given a non-empty, singly linked list with head node head,
+    return a middle node of linked list.
+
+    If there are two middle nodes, return the second middle node.
+    
+    Example 1:
+        Input: [1,2,3,4,5]
+        Output: Node 3 from this list (Serialization: [3,4,5])
+                The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+                Note that we returned a ListNode object ans, such that:
+                ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+    
+    Example 2:
+        Input: [1,2,3,4,5,6]
+        Output: Node 4 from this list (Serialization: [4,5,6])
+                Since the list has two middle nodes with values 3 and 4, we return the second one.
+        
+    Note:
+      1. The number of nodes in the given list will be between 1 and 100.
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curHead = head
+        midHead = head
+        count = 1
+        
+        while curHead is not None:
+            if count%2 == 0:
+                midHead = midHead.next
+            
+            count += 1
+            curHead = curHead.next
+        
+        return midHead
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+"""other faster methods (from other submissions)
+##################################################
+def middleNode(self, head):
+        A = [head]
+        while A[-1].next:
+            A.append(A[-1].next)
+        return A[len(A) / 2]
+"""
