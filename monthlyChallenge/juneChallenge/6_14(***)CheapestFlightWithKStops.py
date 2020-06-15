ce9e14a3a9d2de6eeba1e7c64a95d20bc3fedb66
Phu Tran <andytran1997@gmail.com>
2020-06-15 13:26:32 -0700

    Update and rename 6_14(***) to 6_14(***)CheapestFlightWithKStops.py

--- monthlyChallenge/juneChallenge/6_14(***)CheapestFlightWithKStops.py
+++ monthlyChallenge/juneChallenge/6_14(***)CheapestFlightWithKStops.py
@@ -1,47 +1,66 @@
 # --------------------------------------------------------------------------
-# Name:        Largest Divisible Subset
+# Name:        Cheapest Flights Within K Stops
 # Author(s):   Phu Tran
 # --------------------------------------------------------------------------
 """
-    There are n cities connected by m flights. Each flight starts from city u and arrives at v with a price w.
+    There are n cities connected by m flights. Each flight starts from 
+    city u and arrives at v with a price w.
 
-Now given all the cities and flights, together with starting city src and the destination dst, your task is to find the cheapest price from src to dst with up to k stops. If there is no such route, output -1.
+    Now given all the cities and flights, together with starting city src 
+    and the destination dst, your task is to find the cheapest price from 
+    src to dst with up to k stops. If there is no such route, output -1.
 
-Example 1:
-Input: 
-n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
-src = 0, dst = 2, k = 1
-Output: 200
-Explanation: 
-The graph looks like this:
+    Example 1:
+        Input: 
+        n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+        src = 0, dst = 2, k = 1
+        
+        Output: 200
+        Explanation: 
+        The graph looks like this: (see 6_14_samples.PNG)
+
+        The cheapest price from city 0 to city 2 with at most 1 stop
+        costs 200, as marked red in the picture.
+
+    Example 2:
+        Input: 
+        n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+        src = 0, dst = 2, k = 0
+        
+        Output: 500
+        Explanation: 
+        The graph looks like this: (see 6_14_samples.PNG)
+
+        The cheapest price from city 0 to city 2 with at most 0 stop costs
+        500, as marked blue in the picture.
 
 
-The cheapest price from city 0 to city 2 with at most 1 stop costs 200, as marked red in the picture.
-Example 2:
-Input: 
-n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
-src = 0, dst = 2, k = 0
-Output: 500
-Explanation: 
-The graph looks like this:
-
-
-The cheapest price from city 0 to city 2 with at most 0 stop costs 500, as marked blue in the picture.
- 
-
-Constraints:
-
-The number of nodes n will be in range [1, 100], with nodes labeled from 0 to n - 1.
-The size of flights will be in range [0, n * (n - 1) / 2].
-The format of each flight will be (src, dst, price).
-The price of each flight will be in the range [1, 10000].
-k is in the range of [0, n - 1].
-There will not be any duplicated flights or self cycles.
+    Constraints:
+        1. The number of nodes n will be in range [1, 100], with nodes labeled from 0 to n - 1.
+        2. The size of flights will be in range [0, n * (n - 1) / 2].
+        3. The format of each flight will be (src, dst, price).
+        4. The price of each flight will be in the range [1, 10000].
+        5. k is in the range of [0, n - 1].
+        6. There will not be any duplicated flights or self cycles.
 """
 
-
-#code
-        
+class Solution(object):
+    def findCheapestPrice(self, n, flights, src, dst, K):
+        """
+        :type n: int
+        :type flights: List[List[int]]
+        :type src: int
+        :type dst: int
+        :type K: int
+        :rtype: int
+        """
+        dist = [float("inf")]*n
+        dist[src] = 0
+        for _ in range(K+1):
+            olddist = dist[:]
+            for f in flights:
+                dist[f[1]] = min(dist[f[1]], olddist[f[0]] + f[2])
+        return dist[dst] if dist[dst] < float("inf") else -1
         
         
         
